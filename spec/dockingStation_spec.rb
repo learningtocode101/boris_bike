01f8be4d75ed5bebf6276c0cdcd084fd09e3465f
Shaneil Clarke <shaneilske@yahoo.com>
2018-11-30 16:01:48 +0100

    Changed bike instance to an array and added test

--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -2,22 +2,27 @@ require 'dockingStation'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
   it "gives us a working bike" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.release_bike.working?).to eq true
+    expect(subject.release_bike).to be_working
   end
+
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it "docks a bike" do
-  bike = Bike.new
-  expect(subject.dock(bike)).to eq bike
-end
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq [bike]
+  end
 
   it "raises an error when release_bike and no bike available" do
-    #dock = DockingStation.new
     expect { subject.release_bike }.to raise_error("No Bikes Available")
   end
 
+  it "raises an error when dock and capacity is full" do
+    20.times { subject.dock Bike.new }
+    expect { subject.dock(Bike.new) }.to raise_error("No Space Available")
+  end
 
 end
